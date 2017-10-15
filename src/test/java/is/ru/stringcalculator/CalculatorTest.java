2444510a96bc491a917c139c2990c00b5ca25cb6
eline16 <eline16@ru.is>
2017-10-15 13:41:50 +0000

    Add method now handles new lines between numbers (tested)

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -28,4 +28,11 @@ public class CalculatorTest
 	{
 		assertEquals(6, Calculator.add("1,2,3"));
 	}
+
+	@Test
+	public void testNewLineDelimiter()
+	{
+		assertEquals(6, Calculator.add("1\n2,3"));
+	}
+
 }
